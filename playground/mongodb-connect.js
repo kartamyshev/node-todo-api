3416fcefa30d5baf74e15df4fce8eec465910c17
kartamyshev <kartamyshev.k@yandex.ru>
2017-12-17 13:41:20 +0300

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,37 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient
+  .connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+      console.log('Unable to connect to the MongoDB server.');
+      return;
+    }
+    console.log('Connected to MongoDB server.');
+
+    // db.collection('Todos').insertOne({
+    //   text: 'Watch movie',
+    //   completed: false
+    // }, (err, result) => {
+    //   if (err) {
+    //     console.log('Unable to insert todo', err);
+    //     return;
+    //   }
+    //   console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    // db.collection('Users').insertOne({
+    //   name: 'Konstantin',
+    //   age: 32,
+    //   location: 'Minsk, Belarus'
+    // }, (err, result) => {
+    //   if (err) {
+    //     console.log('Unable to insert user', err);
+    //     return;
+    //   }
+    //   console.log(
+    //     result.ops[0]._id.getTimestamp()
+    //   );
+    // });
+
+    db.close();
+  });
