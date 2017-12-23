9811dbdc7a38e96fd375e466c91c5463ed33616e
kartamyshev <kartamyshev.k@yandex.ru>
2017-12-23 22:10:11 +0300

    setup app for heroku

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const pug = require('pug');
 const { ObjectID } = require('mongodb');
+const port = process.env.PORT || 3000;
 
 const { mongoose } = require('./db/mongoose');
 const { Todo, User } = require('./models');
@@ -60,8 +61,10 @@ app.post('/user/add', (request, response, next) => {
   });
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server is listening on port 3000');
+
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
 
 module.exports = { app };
