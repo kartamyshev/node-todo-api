e966f8a7cd76dd790acfdcd77c01019b771db275
kartamyshev <kartamyshev.k@yandex.ru>
2017-12-23 15:17:44 +0300

    add couple of routes

--- server/server.js
+++ server/server.js
@@ -11,28 +11,6 @@ app.use(bodyParser.json());
 
 app.set("view engine", "pug");
 
-app.post('/todos', (request, response, next) => {
-  const { text } = request.body;
-  const todo = new Todo({ text });
-
-  todo.save().then(doc => {
-    response.send(doc);
-  }, err => {
-    response.status(400).send(err);
-  })
-});
-
-app.post('/users', (request, response, next) => {
-  const { email } = request.body;
-  const user = new User({ email });
-
-  user.save().then(doc => {
-    response.send(doc);
-  }, err => {
-    response.send(err);
-  });
-});
-
 app.get('/', (request, response, next) => {
   const { headers } = request;
   response.render('home', {
@@ -47,11 +25,52 @@ app.get('/', (request, response, next) => {
   });
 });
 
+app.get('/todos/json', (request, response) => {
+  Todo.find()
+    .then(todos => {
+      response.send(todos);
+    }, err => {
+      response.status(400).send(error);
+    });
+});
+
 app.get('/todos', (request, response, next) => {
-  response.render('todos', {
-    title: 'Todos Page'
+  Todo.find().then(todos => {
+    response.render('todos', {
+      title: 'Todos Page',
+      todos: todos
+    });
+  }, (error) => {
+    response.render('home', {
+      error: error
+    });
   });
 });
+
+app.post('/todos', (request, response, next) => {
+  const { text } = request.body;
+  const todo = new Todo({ text });
+
+  todo.save().then(doc => {
+    response.send(doc);
+  }, err => {
+    response.status(400).send(err);
+  })
+});
+
+
+// app.post('/users', (request, response, next) => {
+//   const { email } = request.body;
+//   const user = new User({ email });
+
+//   user.save().then(doc => {
+//     response.send(doc);
+//   }, err => {
+//     response.send(err);
+//   });
+// });
+
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server is listening on port 3000');
