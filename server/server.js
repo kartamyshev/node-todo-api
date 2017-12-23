ccc9627a275a48d492889015cf9517e6c8bb7ae5
kartamyshev <kartamyshev.k@yandex.ru>
2017-12-23 14:13:42 +0300

    fixed port

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ app.get('/todos', (request, response, next) => {
   });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is listening on port 3000');
 });
 
