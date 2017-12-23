e008ff71d44450b4de796a3b30929d9ee9132ec4
kartamyshev <kartamyshev.k@yandex.ru>
2017-12-23 21:16:19 +0300

    add GET todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const pug = require('pug');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo, User } = require('./models');
@@ -11,45 +12,29 @@ app.use(bodyParser.json());
 
 app.set("view engine", "pug");
 
-app.get('/json', (request, response) => {
-  response.send(request.headers);
+app.get('/', (request, response) => {
+  const { headers } = request;
+  response.send(headers);
 });
 
-app.get('/', (request, response, next) => {
-  const { headers } = request;
-  response.render('home', {
-    name: 'Konstantin',
-    title: 'Home Page',
-    headers: Object.keys(headers).map(header => {
-      return {
-        key: header,
-        value: headers[header]
-      }
-    })
+app.get('/todos', (request, response) => {
+  Todo.find().then(todos => {
+    response.send(todos);
   });
 });
 
-app.get('/todos/json', (request, response) => {
-  Todo.find()
-    .then(todos => {
-      response.send(todos);
-    }, err => {
-      response.status(400).send(error);
-    });
-});
+app.get('/todos/:id', (request, response) => {
+  const { id } = request.params;
+  if (ObjectID.isValid(id) === false) {
+    response.status(404).send();
+    return;
+  }
 
-app.get('/todos', (request, response, next) => {
-  const result = {
-    title: 'Todos Page'
-  };
-  Todo.find().then(todos => {
-    result.todos = todos;
-    return result;
-  }, error => {
-    result.error = error;
-    return result;
-  }).then(data => {
-    response.render('todos', data);
+  Todo.findById(id).then(todo => {
+    if (!todo) response.status(404).send();
+    response.send({ todo });
+  }).catch(err => {
+    response.status(400).send();
   });
 });
 
@@ -64,7 +49,6 @@ app.post('/todo/add', (request, response, next) => {
   })
 });
 
-
 app.post('/user/add', (request, response, next) => {
   const { email } = request.body;
   const user = new User({ email });
@@ -75,8 +59,6 @@ app.post('/user/add', (request, response, next) => {
     response.send(err);
   });
 });
-
-
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server is listening on port 3000');
