63e9e4dd0c2b21e49de25ee026776ea04c3f68a3
kartamyshev <kartamyshev.k@yandex.ru>
2018-03-03 16:05:36 +0100

    user model validation

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,36 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const User = mongoose.model('User', {
   email: {
     type: String,
     required: true,
     trim: true,
-    minlength: 15
-  }
+    minlength: 15,
+    unique: true,
+    validate: {
+      validator: (value) => {
+        return validator.isEmail(value);
+      },
+      // validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
 
 module.exports = { User };
